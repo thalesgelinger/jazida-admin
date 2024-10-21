54a0bc8fe817f7ebe95aae27576e7d7fd3a8bd58
Thales Gelinger <thalesgelinger@gmail.com>
2024-10-21 18:44:31 -0300

    update node

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import path from "path";
 
+console.log({ BASE_URL: process.env.BASE_URL })
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [svelte()],
